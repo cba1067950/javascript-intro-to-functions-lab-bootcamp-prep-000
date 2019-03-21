6c9e2747f69aa98f33be2c1ad25d126a0b5d8da6
Joe Jamsky <cba1067950@yahoo.com>
2019-03-21 23:17:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,10 +9,14 @@ function whisper(string){
 function sayHiToGrandma(string){
   var lowercase = string.toLowerCase();
   var uppercase = string.toUpperCase();
+  var youToo = "I love you, Grandma."
   if(string === lowercase){
     return "I can't hear you!"
   } 
   if(string === uppercase){
     return "YES INDEED!"
+  }
+  if(string === youToo){
+    return "I love you, too."
   }
 }
